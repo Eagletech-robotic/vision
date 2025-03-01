53c979dc754aa93619ea0f9ef6118b8d5623a5e8
antmonnier <monniera@gmail.com>
2025-03-01 22:25:30 +0100

    Fix memory leak

--- 10_reconstruct.py
+++ 10_reconstruct.py
@@ -175,6 +175,7 @@ class Reconstruction:
         self.stream_1 = stream_1
         self.stream_2 = stream_2
         self.world_positions = None
+        self.world = World(blocking=False, off_screen=True)
 
     def run(self):
         world_positions_1 = self.stream_1.find_world_positions()
@@ -182,17 +183,17 @@ class Reconstruction:
         self.world_positions = world_positions_1 + world_positions_2
 
     def draw(self):
-        world = World(blocking=False, off_screen=True)
+        self.world.empty()
         for marker_id, x, y, z in self.world_positions:
             if marker_id == vision.MarkerId.TIN_CAN:
-                world.add_tin_can(TinCan(Position(x, y)))
+                self.world.add_tin_can(TinCan(Position(x, y)))
             elif marker_id == vision.MarkerId.ROBOT_BLUE_1 or marker_id == vision.MarkerId.ROBOT_BLUE_2:
                 robot = Robot(Position(x, y, theta=0), RobotColor.BLUE)
-                world.add_robot(robot)
+                self.world.add_robot(robot)
             elif marker_id == vision.MarkerId.ROBOT_YELLOW_1 or marker_id == vision.MarkerId.ROBOT_YELLOW_2:
                 robot = Robot(Position(x, y, theta=0), RobotColor.YELLOW)
-                world.add_robot(robot)
-        return world.render()
+                self.world.add_robot(robot)
+        return self.world.render()
 
 
 def main():
@@ -220,11 +221,12 @@ def main():
         reconstructed = reconstruction.draw()
         img1 = stream_1.draw()
         img2 = stream_2.draw()
-        show_in_fullscreen("Reconstruction", img1, img2, reconstructed)
+        show_in_fullscreen("Reqconstruction", img1, img2, reconstructed)
 
         c = cv.waitKey(50)
         if c == ord("q"):
             running = False
+            cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
