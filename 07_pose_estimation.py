bd217657241fa76fec6be776e22a3d1300481ea0
antmonnier <monniera@gmail.com>
2025-03-01 11:41:36 +0100

    estimate pose moved to a library

--- 07_pose_estimation.py
+++ 07_pose_estimation.py
@@ -4,34 +4,6 @@ import numpy as np
 from lib import common, camera, detection, vision
 
 
-def rodrigues_to_euler(rvec):
-    """Convert Rodrigues rotation vector to Euler angles (in degrees)"""
-    R, _ = cv.Rodrigues(rvec)
-    # Get rotation matrix
-    sy = np.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
-    singular = sy < 1e-6
-
-    if not singular:
-        x = np.arctan2(R[2, 1], R[2, 2])
-        y = np.arctan2(-R[2, 0], sy)
-        z = np.arctan2(R[1, 0], R[0, 0])
-    else:
-        x = np.arctan2(-R[1, 2], R[1, 1])
-        y = np.arctan2(-R[2, 0], sy)
-        z = 0
-
-    # Convert to degrees
-    return np.array([x, y, z]) * 180.0 / np.pi
-
-
-def get_camera_position(rvec, tvec):
-    """Get camera position in world coordinates"""
-    R, _ = cv.Rodrigues(rvec)
-    R = R.T  # Transpose rotation matrix
-    pos = -R @ tvec  # Calculate camera position
-    return pos
-
-
 def main():
     # Select and initialize camera
     common.run_hw_diagnostics()
@@ -61,45 +33,17 @@ def main():
         _, image = cap.read()
         corners, ids, _rejected = aruco_detector.detectMarkers(image)
 
-        if len(corners) > 0:
-            obj_points = []
-            image_points = []
+        for id, corner in zip(ids, corners):
+            cv.drawContours(image, corner.astype(int), -1, (0, 0, 255), 4)
+            cv.circle(image, corner[0][0].astype(int), 8, (0, 0, 255), 8)
+            cv.circle(image, corner[0][1].astype(int), 6, (0, 0, 255), 2)
 
-            for id, corner in zip(ids, corners):
-                cv.drawContours(image, corner.astype(int), -1, (0, 0, 255), 4)
-                cv.circle(image, corner[0][0].astype(int), 8, (0, 0, 255), 8)
-                cv.circle(image, corner[0][1].astype(int), 6, (0, 0, 255), 2)
-
-                if id[0] in known_markers_positions:
-                    image_points.extend(corner[0])
-                    obj_points.extend(known_markers_positions[id[0]])
-
-            obj_points = np.array(obj_points, np.float32)
-            image_points = np.array(image_points, np.float32)
-
-            if len(obj_points) > 0:
-                ret, rvec, tvec = cv.solvePnP(obj_points, image_points, camera_matrix, dist_coeffs)
-                R, _ = cv.Rodrigues(rvec)
-                print("ret", ret)
-                print("rvec", rvec)
-                print("tvec", tvec)
-
-                # Get camera position and rotation
-                camera_pos = get_camera_position(rvec, tvec)
-                print(
-                    f"Camera Position (mm): X={camera_pos[0, 0]:.1f}, Y={camera_pos[1, 0]:.1f}, Z={camera_pos[2, 0]:.1f}")
-
-                camera_rot = rodrigues_to_euler(rvec)
-                print(
-                    f"Camera Rotation (deg): Roll={camera_rot[0]:.1f}, Pitch={camera_rot[1]:.1f}, Yaw={camera_rot[2]:.1f}")
-
-                for id, corner in zip(ids, corners):
-                    if id[0] in known_markers_positions:
-                        continue
-
-                    cv.drawContours(image, corner.astype(int), -1, (255, 0, 0), 4)
-                    cv.circle(image, corner[0][0].astype(int), 8, (255, 0, 0), 8)
-                    cv.circle(image, corner[0][1].astype(int), 6, (255, 0, 0), 2)
+        ret, camera_pos, camera_rot = \
+            vision.estimate_pose(corners, ids, known_markers_positions, camera_matrix, dist_coeffs)
+        if ret:
+            print(f"Camera Position (mm): X={camera_pos[0, 0]:.1f}, Y={camera_pos[1, 0]:.1f}, Z={camera_pos[2, 0]:.1f}")
+            print(
+                f"Camera Rotation (deg): Roll={camera_rot[0]:.1f}, Pitch={camera_rot[1]:.1f}, Yaw={camera_rot[2]:.1f}")
 
         common.show_in_window("image", image)
 
--- lib/vision.py
+++ lib/vision.py
@@ -63,3 +63,51 @@ def compute_homography(corners, ids, known_markers_positions):
     # Compute homography
     H, _ = cv.findHomography(known_corners, known_points_3d[:, :2])
     return H
+
+
+def estimate_pose(corners, ids, known_markers_positions, camera_matrix, dist_coeffs):
+    def get_camera_position(rvec, tvec):
+        """Get camera position in world coordinates"""
+        R, _ = cv.Rodrigues(rvec)
+        R = R.T  # Transpose rotation matrix
+        pos = -R @ tvec  # Calculate camera position
+        return pos
+
+    def rodrigues_to_euler(rvec):
+        """Convert Rodrigues rotation vector to Euler angles (in degrees)"""
+        R, _ = cv.Rodrigues(rvec)
+        # Get rotation matrix
+        sy = np.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+        singular = sy < 1e-6
+
+        if not singular:
+            x = np.arctan2(R[2, 1], R[2, 2])
+            y = np.arctan2(-R[2, 0], sy)
+            z = np.arctan2(R[1, 0], R[0, 0])
+        else:
+            x = np.arctan2(-R[1, 2], R[1, 1])
+            y = np.arctan2(-R[2, 0], sy)
+            z = 0
+
+        # Convert to degrees
+        return np.array([x, y, z]) * 180.0 / np.pi
+
+    obj_points = []
+    image_points = []
+
+    for id, corner in zip(ids, corners):
+        if id[0] in known_markers_positions:
+            obj_points.extend(known_markers_positions[id[0]])
+            image_points.extend(corner[0])
+
+    obj_points = np.array(obj_points, np.float32)
+    image_points = np.array(image_points, np.float32)
+
+    if len(obj_points) > 0:
+        ret, rvec, tvec = cv.solvePnP(obj_points, image_points, camera_matrix, dist_coeffs)
+        camera_pos = get_camera_position(rvec, tvec)
+        camera_rot = rodrigues_to_euler(rvec)
+        return True, camera_pos, camera_rot
+
+    else:
+        return False, None, None
