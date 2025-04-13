55aef5d67ae5258ad7aa43f53c60defea8c0cc19
antmonnier <monniera@gmail.com>
2025-04-13 17:34:23 +0200

    Ultralytics folder org

--- 20_yolo_train.py
+++ 20_yolo_train.py
@@ -1,12 +1,23 @@
+import torch
 from ultralytics import YOLO
+from pathlib import Path
 
-model = YOLO("yolo11m.pt").to("cpu")
+# Before running this script, have the following folders configured in your ~/.config/Ultralytics/settings.json:
+#   "datasets_dir": "/home/antoine/Desktop/robot/vision/yolo-datasets",
+#   "weights_dir": "/home/antoine/Desktop/robot/vision/yolo-runs/weights",
+#   "runs_dir": "/home/antoine/Desktop/robot/vision/yolo-runs",
+
+torch.set_num_threads(8)
+
+model = YOLO("yolo11m-obb.pt").to("cpu")
 print(f"Model loaded on device: {model.device}")
 
-results = model.train(data="yolo-datasets/20250323_01/data.yaml",
-                      epochs=10,
-                      imgsz=640,
-                      batch=16,
-                      project="yolo-runs/train",
-                      name="20250323_01",
-                      exist_ok=True)
+name = "20250323_01-obb"
+results = model.train(
+    data=Path(f"./yolo-datasets/{name}/data.yaml").resolve(),
+    epochs=10,
+    imgsz=640,
+    batch=8,  # 3: max value on NVIDIA GTX 1650
+    name=name,
+    exist_ok=True,
+)
