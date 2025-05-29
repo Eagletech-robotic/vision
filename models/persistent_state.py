4b524f6e2001c904df9a533e1f8e105b2d0db7e1
antmonnier <monniera@gmail.com>
2025-05-29 22:38:04 +0200

    Use last memorised camera pose if it cannot be reliably determined

--- models/persistent_state.py
+++ models/persistent_state.py
@@ -2,3 +2,6 @@ class PersistentState:
     def __init__(self):
         self.team_color = None
         self.score = 40
+
+        # Last reliable poses for each camera
+        self.camera_poses = {}  # camera_index -> (rvec, tvec, pos, euler)
